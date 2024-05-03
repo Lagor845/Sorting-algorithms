7517f0bc0df46de3a35147ff03a0b2a4df39d9a6
Paden O'Neal <Lagor84513@gmail.com>
2024-05-03 11:43:07 -0600

    Initial push

---
+++ Sorting_tester.py
@@ -0,0 +1,21 @@
+import random
+
+class Tester:
+    def generate_list(numbers):
+        num_list = []
+        for i in range(numbers):
+            num_list.append(random.randint(0,numbers))
+        return num_list
+    
+    def test(num_list,num_list_copy):
+        passing = True
+        for i in range(len(num_list_copy)):
+            if num_list[i] != num_list_copy[i]:
+                passing = False
+
+        if passing:
+            print("Passed")
+            print(num_list)
+            
+        else:
+            print("Failed")
